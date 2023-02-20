309f297b7e4a94e0df2e6b9234e3d9b87a6612ad
RyanMB13 <ryan_matthew_bautista@dlsu.edu.ph>
2023-02-20 11:47:30 +0800

    added password checking function

--- BautistaR.c
+++ BautistaR.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
+typedef char String50 [51];
+
+int 
+validPassword(String50 Password){
+    
+    char userInput;
+    if (strlen(Password) == strlen(userInput) && strcmp(Password, userInput) == 0){
+
+        return 1;
+    }
+    else{
+        return 0;
+    }
+} 
+
+
+
+
+int main(){
     
 
     return 0;
