6545626ec3319e1e6e23a008349db01c893e113a
RyanMB13 <ryan_matthew_bautista@dlsu.edu.ph>
2023-04-07 15:18:16 +0800

    added menu display

--- BautistaR.c
+++ BautistaR.c
@@ -2,16 +2,37 @@
 #include <string.h>
 
 typedef char String50 [51];
+ /* displayMainMenu displays the main menu with a user-friendly (text based interface)
+ @returns the players choice among M, P, or E
+ 
+ */
+void displayMainMenu()
+{
+    char cChoice;
+    printf("Please Choose a Mode\n [M]anage Data\n [P]lay\n [E]xit\n");
+    scanf("%c", &cChoice);
+    do
+    {
+        if (cChoice != 'M' && cChoice != 'P' && cChoice != 'E')
+        {
+            printf("Invalid Input!\n");
+        }
+    } while (cChoice != 'M' && cChoice != 'P' && cChoice != 'E');
+    return cChoice;
+}
+
 
 int 
 validPassword(String50 Password){
     
     char userInput;
-    if (strlen(Password) == strlen(userInput) && strcmp(Password, userInput) == 0){
+    if (strlen(Password) == strlen(userInput) && strcmp(Password, userInput) == 0)
+    {
 
         return 1;
     }
-    else{
+    else
+    {
         return 0;
     }
 } 
@@ -20,7 +41,15 @@ validPassword(String50 Password){
 
 
 int main(){
-    
+    String50 sMode;
+
+
+
+
+
+
+
+
 
     return 0;
 }
