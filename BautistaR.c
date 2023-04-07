3590d059d6009882c855b1ab192b0683e1610df4
RyanMB13 <ryan_matthew_bautista@dlsu.edu.ph>
2023-04-07 15:56:11 +0800

    tested the menus and input choices

--- BautistaR.c
+++ BautistaR.c
@@ -2,16 +2,19 @@
 #include <string.h>
 
 typedef char String50 [51];
+
  /* displayMainMenu displays the main menu with a user-friendly (text based interface)
  @returns the players choice among M, P, or E
  */
+
 char displayMainMenu()
 {
     char cChoice;
-    printf("Please choose a mode:\n [M]anage Data\n [P]lay\n [E]xit\n");
-    scanf("%c", &cChoice);
+    printf("Please choose a mode:\n [M]anage Data as an admin\n [P]lay the game as a player\n [E]xit the program\n");
     do
     {
+        printf("Enter your choice: \n");
+        scanf(" %c", &cChoice);
         if (cChoice != 'M' && cChoice != 'P' && cChoice != 'E')
         {
             printf("Invalid input!\n");
@@ -21,6 +24,27 @@ char displayMainMenu()
 }
 
 
+ /* displayMainMenu displays the main menu with a user-friendly (text based interface)
+ @returns the players choice among P, V, or E
+ */
+
+char displayPlayMenu()
+{
+    char cChoice;
+    printf("Please choose a mode:\n [P]lay\n [V]iew Scores\n [E]xit\n");
+    do
+    {
+        printf("Enter your choice: \n");
+        scanf(" %c", &cChoice);
+        if (cChoice != 'P' && cChoice != 'V' && cChoice != 'E')
+        {
+            printf("Invalid input!\n");
+        }
+    } while (cChoice != 'P' && cChoice != 'V' && cChoice != 'E');
+    return cChoice;
+}
+
+/*
 int 
 validPassword(String50 Password){
     
@@ -35,23 +59,34 @@ validPassword(String50 Password){
         return 0;
     }
 } 
-
+*/
 
 
 
 int main()
 {
-    char cMode;
+    char cMode, cPlayMode;
     while(cMode != 'E')
     {
         cMode = displayMainMenu();
         if (cMode == 'M')
         {
-
+            printf("You chose Manage Data!\n");
         }
         if (cMode == 'P')
         {
-
+            do
+            {
+                cPlayMode = displayPlayMenu();
+                if(cPlayMode == 'P')
+                {
+                    printf("You chose Play!\n");
+                }
+                if(cPlayMode == 'V')
+                {
+                    printf("You chose View Scores!\n");
+                }
+            } while (cPlayMode != 'E');
         }
     }
     return 0;
